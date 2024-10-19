f5b32a71a6a485ff5d37a6ccaf90fc2d49c463d6
zakriyakhandeveloper <coding4443@gmail.com>
2024-10-19 10:24:09 -0700

    Initial commit of e commerce app

--- config/stripeconfig.js
+++
@@ -1,3 +0,0 @@
-console.log('Stripe Secret Key:',"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"); // Debugging line
-const stripe = require('stripe')("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");
-module.exports = stripe;
